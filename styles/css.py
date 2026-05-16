5ae3b83373d960fad4f6a3255625d0230bc1dc2d
Jana Mairal <mairalsanchez@gmail.com>
2026-05-16 20:45:02 +0200

    Update css.py

--- styles/css.py
+++ styles/css.py
@@ -147,7 +147,7 @@ def inject_global_css() -> None:
         padding: 1.1rem 1.15rem 0.9rem 1.15rem;
         box-shadow: 0 4px 20px rgba(0,0,0,0.05);
         min-height: 115px;
-        overflow: hidden !important;
+        overflow: visible !important;
     }}
 
     [data-testid="stHorizontalBlock"] > div:first-child [data-testid="stMetric"] {{
